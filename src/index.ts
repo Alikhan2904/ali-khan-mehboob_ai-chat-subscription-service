7c4d3bf8aecdad4bc83b7f3576fb4e34393f7969
Ali Khan Mehboob <alikhan.sachwani@gmail.com>
2025-07-24 03:58:40 +0500

    init commit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import chatRouter from "./chat/controllers/chatController";
+import subscriptionRouter from "./subscriptions/controllers/subscriptionController";
+
+const app = express();
+
+app.use(express.json());
+app.use("/chat", chatRouter);
+app.use("/subscriptions", subscriptionRouter);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
